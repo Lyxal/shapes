a45c9fd43dba43e00ebd94daef87d2b66f0db59c
PhotonNikko <100416531+interrrrobang@users.noreply.github.com>
2022-03-05 10:51:58 +0700

    idk

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='shaapes',
-    version='0.0.0.dev0',
+    version='0.0.0.dev1',
     description='A graphical shape based esolang',
     long_description=long_description,
     long_description_content_type='text/markdown',
